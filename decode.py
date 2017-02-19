827600e65bd8e45b3f5eb4f6f384dfa309e96873
sasham43 <sashasemail@gmail.com>
2017-02-19 14:18:33 -0600

    info from 2

--- decode.py
+++ decode.py
@@ -24,7 +24,7 @@ RATE      = 9600
 CHUNKSIZE = 1024
 
 p = pyaudio.PyAudio()
-devinfo = p.get_device_info_by_index(1)  # Or whatever device you care about.
+devinfo = p.get_device_info_by_index(2)  # Or whatever device you care about.
 if p.is_format_supported(44100.0,  # Sample rate
                          input_device=devinfo['index'],
                          input_channels=devinfo['maxInputChannels'],
