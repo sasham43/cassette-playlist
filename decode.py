02039b3474526d94be888fcd9f9a38d36e94828c
sasham43 <sashasemail@gmail.com>
2017-02-19 14:18:59 -0600

    actually test rate

--- decode.py
+++ decode.py
@@ -25,7 +25,7 @@ CHUNKSIZE = 1024
 
 p = pyaudio.PyAudio()
 devinfo = p.get_device_info_by_index(2)  # Or whatever device you care about.
-if p.is_format_supported(44100.0,  # Sample rate
+if p.is_format_supported(RATE,  # Sample rate
                          input_device=devinfo['index'],
                          input_channels=devinfo['maxInputChannels'],
                          input_format=pyaudio.paInt16):
